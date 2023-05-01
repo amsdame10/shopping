8272c22b5939a21da9ff95fccefb3ef5a3ea3d76
amsdame10 <122823920+amsdame10@users.noreply.github.com>
2023-04-30 21:31:36 -0700

    Added html edits/ Edited details-main.js/ data.js/css

--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,21 @@
 let shopItemsData = [
   {
     id: "jfhgbvnscs",
-    name: "get your cray on",
+    name: "Get your cray on",
     price: 27,
     desc: "Crayon childs hair clip.",
     img: "images/crayons clip.png",
   },
   {
     id: "ioytrhndcv",
-    name: "Office Shirt",
+    name: "happy clip",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/happy clip Small.jpg",
   },
   {
     id: "wuefbncxbsn",
-    name: "T Shirt",
+    name: "sunnies",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
     img: "images/Screen Shot 2023-04-27 at 10.53.39 AM.png",
