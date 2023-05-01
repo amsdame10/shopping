7d1f336a3559d99816716c635df5b4fb18c78b46
amsdame10 <122823920+amsdame10@users.noreply.github.com>
2023-04-30 18:21:12 -0700

    Changed title and h2 on cart and index/ updated desc and css

--- src/Data.js
+++ src/Data.js
@@ -1,24 +1,24 @@
 let shopItemsData = [
   {
     id: "jfhgbvnscs",
-    name: "Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/",
+    name: "get your cray on",
+    price: 27,
+    desc: "Crayon childs hair clip.",
+    img: "images/crayons clip.png",
   },
   {
     id: "ioytrhndcv",
     name: "Office Shirt",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-2.jpg",
+    img: "images/happy clip Small.jpg",
   },
   {
     id: "wuefbncxbsn",
     name: "T Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/img-3.jpg",
+    img: "images/Screen Shot 2023-04-27 at 10.53.39 AM.png",
   },
   {
     id: "thyfhcbcv",
